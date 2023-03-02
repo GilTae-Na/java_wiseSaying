d45a41d4f36dcb78f2ce11b9a7a18b5b95e6ee3e
nagiltae <nagt1997@gmail.com>
2023-03-02 09:18:18 +0900

    1단계

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("==명언 앱==");
+        System.out.printf("명령)  ");
+        String order = sc.nextLine();
+    }
+}
+
+
