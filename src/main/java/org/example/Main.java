2038baa8adf87e6bc4dfa2721dad184e77f4511e
nagiltae <nagt1997@gmail.com>
2023-03-02 09:24:25 +0900

    2단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,40 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        List<Myung> mList = new ArrayList<>();
+        String say; String person;
         System.out.println("==명언 앱==");
-        System.out.printf("명령)  ");
-        String order = sc.nextLine();
+
+        while (true) {
+            System.out.printf("명령)  ");
+            String order = sc.nextLine();
+
+            if (order.equals("종료")) {
+                break;
+            } else if (order.equals("등록")) {
+                System.out.print("명언: ");
+                say = sc.nextLine();
+                System.out.print("작가: ");
+                person = sc.nextLine();
+                mList.add(new Myung(say, person));
+            }
+           else {
+               System.out.println("올바른 코드를 입력하세요");
+               sc.nextLine();
+           }
+
+        }
     }
 }
 
+class Myung{
+
+    private String say;
+    private  String person;
+
+    Myung(String say, String person){
+        this.say = say;
+        this.person = person;
+    }
+}
 
